1c0747c38477ff949b2fab7dbfcb0c46906edf34
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-03-27 22:23:26 -0300

    adicionando novos algoritmos de ordenação

---
+++ 1 - General Development Skills/Estrutura de dados e algoritmos/QuickSort/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+
+        
+
+    }
+}
---
+++ 1 - General Development Skills/Estrutura de dados e algoritmos/SelectionSort/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        //selection sort
+        public static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
+    }
+}
