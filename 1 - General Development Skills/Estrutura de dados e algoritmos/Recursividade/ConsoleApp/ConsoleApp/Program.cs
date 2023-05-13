ff66ee3bca1d2dc7601a1a5705bdb8770a4b19c6
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-05-13 13:47:58 -0300

    função recursiva

---
+++ 1 - General Development Skills/Estrutura de dados e algoritmos/Recursividade/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Regressive(5);
+
+            Console.ReadLine();
+        }
+
+        public static void Regressive(int n)
+        {
+            Console.WriteLine(n);
+            if(n <= 0)
+            {
+                return;
+            }
+            else
+            {
+                Regressive(n - 1);
+            }
+        }
+    }
+}
